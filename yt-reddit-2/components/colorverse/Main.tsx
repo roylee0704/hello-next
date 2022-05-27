2147f1a8a8b7cc73b84daf9cf35d604cff9dfb62
roylee0704 <roylee0704@gmail.com>
2022-05-28 02:15:02 +0800

    cool

---
+++ yt-reddit-2/components/colorverse/Main.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="absolute top-0 left-[21rem] right-0 flex h-screen  flex-1 flex-col border-4 border-green-600 pt-20">
+      <div className="h-full overflow-y-auto border-4 border-yellow-500">
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>side</p>
+        <p>side</p>
+
+        <p>side</p>
+      </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ yt-reddit-2/components/colorverse/Side.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Side = () => {
+  return (
+    <div className="absolute top-0 z-50 flex h-screen min-w-[20rem] flex-col border-4 border-purple-600 pt-20">
+      <div className="h-full overflow-y-auto border-4 border-yellow-500">
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+        <p>side</p>
+
+        <p>side</p>
+      </div>
+    </div>
+  )
+}
+
+export default Side
