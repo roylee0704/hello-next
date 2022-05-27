df87ef89e6decdc7579559d9141b878130c79ab5
roylee0704 <roylee0704@gmail.com>
2022-05-28 02:29:52 +0800

    cool

--- yt-reddit-2/components/colorverse/Main.tsx
+++ yt-reddit-2/components/colorverse/Main.tsx
@@ -2,8 +2,40 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className="absolute top-0 left-[21rem] right-0 flex h-screen  flex-1 flex-col border-4 border-green-600 pt-20">
-      <div className="h-full overflow-y-auto border-4 border-yellow-500">
+    <div className="absolute top-0 left-0 right-0 h-screen border-4 border-green-600 pt-20 md:left-[21rem]">
+      <div className="h-full flex-1 flex-col overflow-y-auto border-4 border-yellow-500 bg-yellow-300">
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
+        <p>content............</p>
         <p>content............</p>
         <p>content............</p>
         <p>content............</p>
--- yt-reddit-2/components/colorverse/Side.tsx
+++ yt-reddit-2/components/colorverse/Side.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Side = () => {
   return (
-    <div className="absolute top-0 z-50 flex h-screen min-w-[20rem] flex-col border-4 border-purple-600 pt-20">
-      <div className="h-full overflow-y-auto border-4 border-yellow-500">
+    <div className="hidden md:absolute md:top-0 md:z-50 md:block md:h-screen md:min-w-[20rem] md:border-4 md:border-purple-600 md:pt-20">
+      <div className="flex h-full flex-col overflow-y-auto border-4 border-yellow-500">
         <p>side</p>
         <p>side</p>
         <p>side</p>
