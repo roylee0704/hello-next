646326b6f6dc55f62e23768c2513f67cee9946dd
roylee0704 <roylee0704@gmail.com>
2022-05-28 10:11:30 +0800

    commit

--- yt-reddit-2/pages/index.tsx
+++ yt-reddit-2/pages/index.tsx
@@ -15,7 +15,13 @@ const Home: NextPage = () => {
         </Link>
         <Link href="/colorverse">
           <span className="min-w-[10rem] cursor-pointer border-4 border-red-400 px-4 py-2 text-center">
-            View Colorverse
+            View Colorverse (absolute)
+          </span>
+        </Link>
+
+        <Link href="/colorverse-flex">
+          <span className="min-w-[10rem] cursor-pointer border-4 border-red-400 px-4 py-2 text-center">
+            View Colorverse (flex)
           </span>
         </Link>
         <Link href="/blank">
