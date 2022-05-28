524b6f84a191661ccd6f07b86bd1617d6ba0351f
roylee0704 <roylee0704@gmail.com>
2022-05-28 16:04:41 +0800

    tiktok

--- yt-reddit-2/pages/index.tsx
+++ yt-reddit-2/pages/index.tsx
@@ -24,6 +24,11 @@ const Home: NextPage = () => {
             View Colorverse (flex)
           </span>
         </Link>
+        <Link href="/tiktok">
+          <span className="min-w-[10rem] cursor-pointer border-4 border-red-400 px-4 py-2 text-center">
+            View TikTok
+          </span>
+        </Link>
         <Link href="/blank">
           <span className="min-w-[10rem] cursor-pointer border-4 border-red-400 px-4 py-2 text-center">
             View Blank
